b8cb142ccebfeb2568c5b446a4d368338afd4b81
ryan-hasty <rhastydev@gmail.com>
2024-04-01 18:46:31 -0500

    [hastyrr] change stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
 import { AppComponent} from "./app.component";
 
-export const routes: Routes = [
-  { path: '/allstar-web/', component: AppComponent}
-
-];
+export const routes: Routes = [];
