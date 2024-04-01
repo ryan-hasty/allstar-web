f79a2c3f8b6d1d2f5b5d56ba89f4938a74a20709
ryan-hasty <rhastydev@gmail.com>
2024-04-01 18:42:13 -0500

    [hastyrr] configure route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { AppComponent} from "./app.component";
 
 export const routes: Routes = [
-  { path: '', component: AppComponent}
+  { path: '/allstar-web/', component: AppComponent}
 
 ];
